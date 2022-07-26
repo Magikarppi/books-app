025376c87de9fc9fcb945d44e63d1e0751a2b72a
Sami H <samih90@gmail.com>
2022-07-26 19:59:47 +0300

    Add code for adding new books with POST to the routes-controller

--- backend/controllers/books.ts
+++ backend/controllers/books.ts
@@ -4,8 +4,23 @@ import Book from '../models/book';
 const router = express.Router();
 
 router.get('/', async (_req: Request, res: Response) => {
-  const books = await Book.findAll();
-  res.json(books);
+  try {
+    const books = await Book.findAll();
+    res.json(books);
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const book = await Book.create(req.body);
+    res.json(book);
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).send({ error: error.message });
+  }
 });
 
 module.exports = router;
