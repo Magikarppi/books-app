239cc6e3ea369a8bb677722c31f6e20bca708295
Sami H <samih90@gmail.com>
2022-07-27 15:39:35 +0300

    Show BookList.tsx component and pass books-dummy-data to it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import BookForm from './components/BookForm';
+import BookList from './components/BookList';
+
+const booksDummyData: BookType[] = [
+  {
+    id: 1,
+    bookName: 'The Gunslinger',
+    author: 'Stephen King',
+    description: 'a nice book',
+  },
+];
 
 function App() {
   const handleBookFormSubmit = (
@@ -11,6 +21,7 @@ function App() {
     <div className="App">
       <div className="ContentContainer">
         <BookForm handleBookFormSubmit={handleBookFormSubmit} />
+        <BookList books={booksDummyData} />
       </div>
     </div>
   );
