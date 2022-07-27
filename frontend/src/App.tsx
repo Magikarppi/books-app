8f6d0d527928e1bb260c3e8a1d8deca3916fec5c
Sami H <samih90@gmail.com>
2022-07-27 15:47:28 +0300

    Add key-prop to map and update books-dummy-data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,18 @@ const booksDummyData: BookType[] = [
     author: 'Stephen King',
     description: 'a nice book',
   },
+  {
+    id: 2,
+    bookName: 'Atomic Habits',
+    author: 'James Clear',
+    description: 'for ppl who want to get s?it done',
+  },
+  {
+    id: 3,
+    bookName: 'Bhagavad Gita',
+    author: 'Vyasa',
+    description: 'Epic classic',
+  },
 ];
 
 function App() {
--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -2,7 +2,7 @@ const BookList = ({ books }: BookListProps) => {
   return (
     <div className="BookListContainer">
       {books.map((book) => (
-        <div className="BookBlock">
+        <div className="BookBlock" key={book.id}>
           <div className="BookElement">{book.bookName}</div>
           <div className="BookElement">{book.author}</div>
         </div>
