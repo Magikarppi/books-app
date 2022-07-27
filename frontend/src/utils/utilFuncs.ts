4a32672d40e6319033bed6eae5f2e7635f263076
Sami H <samih90@gmail.com>
2022-07-27 20:47:56 +0300

    Add custom fetch function
     - Shorten the timeout to 12s
     - Abort request if it takes too long

---
+++ frontend/src/utils/utilFuncs.ts
@@ -0,0 +1,16 @@
+export const fetchWithTimeout = async (url: string, options?: RequestInit) => {
+  const timeout = 12000;
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const ticToc = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(url, {
+    ...options,
+    signal,
+  });
+  clearTimeout(ticToc);
+
+  return response;
+};
