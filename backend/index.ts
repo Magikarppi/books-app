a8df9b390dfbfce021006d04854fb0f741bd3d63
Sami H <samih90@gmail.com>
2022-07-27 21:35:54 +0300

    Install and apply cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 const { PORT } = require('./utils/config');
 const { connectToDatabase } = require('./utils/db');
 const booksRouter = require('./controllers/books');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/books', booksRouter);
 
